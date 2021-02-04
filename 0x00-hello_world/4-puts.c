42674b507ecabbe07fcaa5e91163f7fd124bc9ec
Your Name <iso.mora.cor@gmail.com>
2021-02-04 03:55:58 +0000

    help

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - function prints the required text and returns 0
+* Return: 0
+*/
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main - function prints the required text and returns 0
+* Return: 0
+*/
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+
+}
