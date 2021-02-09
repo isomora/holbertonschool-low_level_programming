81e5367415358568bbec4e74181a527abc4758bf
Your Name <iso.mora.cor@gmail.com>
2021-02-09 17:10:28 +0000

    hi

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,15 +1,7 @@
-#include <unistd.h>
-
-/**
- * _putchar - writes the character c to stdout
- * @c: Holberton
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-	return (0);
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("Holberton");
+   return 0;
 }
 
